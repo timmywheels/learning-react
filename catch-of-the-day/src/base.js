34d22466254bb87724d0afb24228370f859a92f9
Tim <tim@timwheeler.com>
2018-07-15 15:32:27 -0400

    removed api keys

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyB0bF7ulk5unDPgrLzVkioPFYCQNe13gAQ",
-    authDomain: "catch-of-the-day-timwheeler.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-timwheeler.firebaseio.com"
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    databaseURL: "DATABASE_URL"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
